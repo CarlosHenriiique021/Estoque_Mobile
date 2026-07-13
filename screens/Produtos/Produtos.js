1f019041fc4600924c1ccf3d5c865883a2a8a6b8
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-13 19:48:10 -0300

    V1.0

---
+++ screens/Produtos/Produtos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity } from 'react-native';
+import { Picker, ScrollView } from 'react-native-web';
+import { useNavigation } from '@react-navigation/native';
+import { styles } from '../../style';
+
+export default function Produtos() {
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <Text>Produtos</Text>
+        </View>
+    )
+};
+
+
---
+++ style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b4b4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  button: {
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  }
+});
