c39958fe97be4ebc511022aae0799d144195ad6e
Matheus Matos <matheus2003tvhd2m@gmail.com>
2026-07-15 17:13:45 -0300

    implementação da tela de cadastro de usuarios

--- screens/CadastroUsuario/CadastroUsuario.js
+++ screens/CadastroUsuario/CadastroUsuario.js
@@ -1,15 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity } from 'react-native';
-import { Picker, ScrollView } from 'react-native-web';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from '../../style';
 
 export default function CadastroUsuario({navigation}) {
 
     return (
-        <View style={styles.viewPrincipal}>
-            
+    <View style={styles.viewPrincipal}>
+
+        <View style = {styles.subtitulo} >
+            <Text style = {styles.textosubtitulo}>
+                Criar conta
+            </Text>
         </View>
+            <Text style = {styles.texto}>Nome</Text>
+
+            <TextInput
+             style = {styles.input}   
+            placeholder='Digite seu nome completo'
+            placeholderTextColor= 'gray'
+            />
+
+            <Text style = {styles.texto}>E-mail</Text>
+
+            <TextInput
+            style = {styles.input}  
+            placeholder='Digite seu e-mail'
+            placeholderTextColor= 'gray'
+          
+            />
+
+            <Text style = {styles.texto}>Senha</Text>
+
+            <TextInput
+            style = {styles.input}  
+            placeholder='Mínimo 6 caracteres'
+            placeholderTextColor= 'gray'
+            />
+
+            <Text style = {styles.texto}>Confirmar senha</Text>
+
+            <TextInput
+            style = {styles.input}  
+            placeholder='Confirme sua senha'
+            placeholderTextColor= 'gray'
+            />
+     
+        
+            <TouchableOpacity style = {styles.button}>
+                <Text style = {styles.textoButton}>
+                    Cadastrar
+                </Text>
+            </TouchableOpacity>
+   
+            <View style = {styles.arealogin}>
+                <Text style = {styles.textoConta}>
+                    Já tem uma conta?
+                </Text>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                    <Text style={styles.login}>
+                        Login
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+    </View>  
     )
 };
-
--- style.js
+++ style.js
@@ -8,16 +8,60 @@ export const styles = StyleSheet.create({
   viewPrincipal: {
     flex: 1,
     backgroundColor: colors.primary,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 
-  texto1: {
-    color: 'red',
-    fontSize: 90
-  },
-
-   texto2: {
+  texto: {
     color: 'white',
-    fontFamily: 'GoogleSans_500Medium '
+    fontFamily: 'poppins',
+    fontWeight: 'semibold',
+    padding: 5,
+    marginTop: 5
+  },
+  input: {
+    backgroundColor: colors.primary,
+    height: 50,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#6b6b6be1',
+  },
+  button: {
+ backgroundColor: '#2563eb',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  textoButton: {
+    color: 'whitesmoke',
+    textAlign: 'center',
+    fontFamily: 'poppins',
+    fontSize: 25
+  },
+  arealogin: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 25,
+    gap: 5,
+  },
+  login: {
+    color: '#2563eb',
+    fontWeight: 'bold',
+  },
+  textoConta: {
+    color: 'whitesmoke',
+  },
+  textosubtitulo: {
+    fontSize: 35,
+    color: '#ffffff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitulo: {
+    marginTop: -10,
+    paddingHorizontal: 20,
   }
 });
