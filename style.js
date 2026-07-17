7006fb05fa3caf9b3e58d6e494432f980de3de40
Carlos Henrique de Araujo Nunes <vic0267540@aluno.firjansenaisesi.com.br>
2026-07-16 21:08:04 -0300

    feat: implementacao da tela de login

--- style.js
+++ style.js
@@ -1,23 +1,90 @@
 import { StyleSheet } from 'react-native';
 
 const colors = {
-  primary: '#01071b',    // Azul Escuro Profissional
+  primary: '#ffffff',
 };
 
 export const styles = StyleSheet.create({
+
   viewPrincipal: {
     flex: 1,
-    backgroundColor: colors.primary,
-    justifyContent: 'center'
+    backgroundColor: '#fff',
+    paddingHorizontal: 30,
+    justifyContent: 'flex-start',
+    paddingTop: 20,
   },
 
-  texto1: {
-    color: 'red',
-    fontSize: 90
+  textoLogo: {
+    fontSize: 30,
+    color: '#0057b8',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5,
   },
 
-   texto2: {
-    color: 'white',
-    fontFamily: 'GoogleSans_500Medium '
-  }
+  textoLogin: {
+    fontSize: 16,
+    color: '#6b7280',
+    fontWeight: '600',
+    textAlign: 'center',
+    marginBottom: 25,
+  },
+
+  textoLogin1: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#d1d5db',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    color: '#000000',
+    backgroundColor: '#ffffff',
+    marginBottom: 15,
+  },
+
+  texto: {
+    color: '#111827',
+    fontSize: 15,
+    fontWeight: '600',
+    marginBottom: 6,
+  },
+
+  button: {
+    backgroundColor: '#2563eb',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  textoButton: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  areaCadastro: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 25,
+    gap: 5,
+  },
+
+  textoConta: {
+    color: '#6b7280',
+  },
+
+  cadastro: {
+    color: '#2563eb',
+    fontWeight: 'bold',
+  },
+
+  logotipo: {
+    width: 180,
+    height: 180,
+    alignSelf: 'center',
+    marginBottom: 5,
+  },
+
 });
