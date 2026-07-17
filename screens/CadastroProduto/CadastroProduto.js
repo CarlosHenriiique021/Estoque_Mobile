083dea42b163acfb0d071c6b393520bd3cf8c7d6
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-16 21:14:22 -0300

    CadastroProduto_FinalizadoFront

--- screens/CadastroProduto/CadastroProduto.js
+++ screens/CadastroProduto/CadastroProduto.js
@@ -1,16 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity, TextInput } from 'react-native';
 import { Picker, ScrollView } from 'react-native-web';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from '../../style';
 
-export default function CadastroProduto({navigation}) {
+export default function CadastroProduto({ navigation }) {
 
     return (
-        <View style={styles.viewPrincipal}>
-            
+        <View style={styles.container}>
+
+            <View style={styles.container2}>
+                <View style={styles.container}>
+
+                    <View style={styles.CotainerImagem}>
+                        <Image
+                            source={require('../../assets/local-logoTextBranco.png')}
+                            style={styles.logo}
+                        />
+                    </View>
+
+                    <Text style={styles.tituloCadastroProduto}>Cadastro de Produto</Text>
+                </View>
+            </View>
+
+            <View>
+                <View style={styles.viewContainer}>
+                    <Text style={styles.textoPrincipal}>Nome</Text>
+                    <TextInput style={styles.textoInput} placeholder='Nome do produto' placeholderTextColor="rgba(255, 255, 255, 0.3)"></TextInput>
+                </View>
+
+                <View style={styles.viewContainer}>
+                    <Text style={styles.textoPrincipal}>Categoria</Text>
+                    <TextInput style={styles.textoInput} placeholder='Digite a categoria...' placeholderTextColor="rgba(255, 255, 255, 0.3)"></TextInput>
+                </View>
+
+                <View style={styles.viewContainer}>
+                    <Text style={styles.textoPrincipal}>Quantidade</Text>
+                    <TextInput style={styles.textoInput} placeholder='Digite a quantidade...' placeholderTextColor="rgba(255, 255, 255, 0.3)"></TextInput>
+                </View>
+
+                <View style={styles.viewContainer}>
+                    <Text style={styles.textoPrincipal}>Valor</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={styles.textoCifrao}>R$</Text>
+                        <TextInput style={styles.textoInputValor} placeholder='Digite o valor...' placeholderTextColor="rgba(255, 255, 255, 0.6)"></TextInput>
+                    </View>
+                </View>
+
+                <TouchableOpacity>
+                    <View style={styles.btn_Salvar}>
+                        <Text style={styles.btn_SalvarText}> SALVAR </Text>
+                    </View>
+                </TouchableOpacity>
+
+
+                <View style={styles.viewEditarELista}>
+                    <TouchableOpacity>
+                        <Text style={styles.btn_editar}> EDITAR </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.btn_lista}> LISTAR </Text>
+
+                    </TouchableOpacity>
+                </View>
+
+            </View>
         </View>
-    )
+    );
 };
 
 
--- style.js
+++ style.js
@@ -1,23 +1,132 @@
 import { StyleSheet } from 'react-native';
 
 const colors = {
-  primary: '#01071b',    // Azul Escuro Profissional
+  primary: '#0D1b2A',
+  secondary: '#364a66',
+  button: '#1E88E5'
 };
 
 export const styles = StyleSheet.create({
-  viewPrincipal: {
+
+  container: {
     flex: 1,
     backgroundColor: colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  container2: {
+    marginTop: -110,
+    marginBottom: 30
+  },
+
+
+
+  logo: {
+    width: 300,
+    height: 300,
+  },
+
+  tituloCadastroProduto: {
+    fontWeight: '700',
+    color: 'white',
+    fontSize: 33,
+    marginTop: -90,
+    marginBottom: -20
+  },
+
+  viewContainer: {
+    width: 320,
+    margin: 5,
+  },
+
+  textoPrincipal: {
+    fontSize: 17,
+    fontWeight: 500,
+    color: 'white',
+    paddingLeft: 2,
+    marginBottom: 3,
+    paddingTop: 10
+  },
+
+  textoCifrao: {
+    fontSize: 17,
+    padding: 10,
+    backgroundColor: colors.secondary,
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
+    marginTop: 2,
+    borderWidth: 1,
+    borderColor: 'black',
+    color: 'white'
+  },
+
+  textoInput: {
+    fontSize: 17,
+    paddingVertical: 10,
+    borderRadius: 5,
+    paddingLeft: 2,
+    marginTop: 2,
+    color: 'white',
+    backgroundColor: colors.secondary,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+
+  textoInputValor: {
+    fontSize: 17,
+    paddingVertical: 10,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    paddingLeft: 2,
+    paddingRight: 80,
+    marginTop: 2,
+    color: 'white',
+    backgroundColor: colors.secondary,
+    borderWidth: 1,
+    borderLeftWidth: -1,
+    borderColor: 'black',
+  },
+
+  btn_Salvar: {
+    backgroundColor: colors.button,
+    width: 320,
+    margin: 5,
+    paddingVertical: 10,
+    borderRadius: 8,
+    marginTop: 50
+  },
+
+  btn_SalvarText: {
+    fontSize: 15,
+    fontWeight: 700,
+    textAlign: 'center',
+    color: 'white'
+  },
+
+  viewEditarELista: {
+    flexDirection: 'row',
+    gap: 5,
     justifyContent: 'center'
   },
 
-  texto1: {
-    color: 'red',
-    fontSize: 90
+  btn_editar: {
+    backgroundColor: 'gray',
+    fontSize: 15,
+    fontWeight: 700,
+    paddingVertical: 10,
+    paddingHorizontal: 49,
+    borderRadius: 8,
+    color: 'white'
   },
 
-   texto2: {
-    color: 'white',
-    fontFamily: 'GoogleSans_500Medium '
+  btn_lista: {
+    backgroundColor: 'gray',
+    fontSize: 15,
+    fontWeight: 700,
+    paddingVertical: 10,
+    paddingHorizontal: 49,
+    borderRadius: 8,
+    color: 'white'
   }
 });
